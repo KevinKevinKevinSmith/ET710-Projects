9d80dceaf97d47f003274b25d324ee4cc8538941
KevinKevinKevinSmith <KStar0715@gmail.com>
2015-01-24 11:52:27 -0500

    Project 9 stuff
    Doing Project 9, making a basic form using angularjs

---
+++ app.js
@@ -0,0 +1,12 @@
+angular.module('Project', [])
+    .controller('FormController', function($scope) {
+		user.name = "name";
+		};
+	);
+	
+	var value = element(by.binding('value | date: "yyyy-MM-dd"'));
+/*
+form.submit() {
+go("./Project_Success.htm")
+}
+*/
